5b3443568d6cec0caa1341b95aec6c95cdce231a
Dax Raad <d@ironbay.co>
2024-05-06 11:49:48 -0400

    fix packagename

--- provider/resources.go
+++ provider/resources.go
@@ -149,7 +149,7 @@ func Provider() tfbridge.ProviderInfo {
 			// },
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
-			PackageName: "@sst-provider/pulumi",
+			PackageName: "@sst-provider/supabase",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
